d896457db8ce463e800bedb1c40182884ec7cd8d
Gonzalo <lamasgonzaloezequiel@gmail.com>
2025-12-03 20:01:25 -0300

    tests cart and checkout

--- data/data_login.py
+++ data/data_login.py
@@ -1,5 +1,5 @@
 CASOS_LOGIN = [
     ("standard_user", "secret_sauce", True),     # usuario válido, login exitoso
     ("locked_out_user", "secret_sauce", False),  # usuario bloqueado, login falla
-    ("usuario_malo", "password_malo", False),    # credenciales inválidas, login falla
+    ("usuario_falso", "password_false", False),    # credenciales inválidas, login falla
 ]
